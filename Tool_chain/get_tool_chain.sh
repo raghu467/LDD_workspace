c4c77f2c45856906323ab8ffd15e5ae7e24b14d8
raghugoud@nyu.edu <raghavender.yadagiri@magna.com>
2023-06-23 12:06:25 -0700

    added setup toolchain sh

---
+++ Tool_chain/get_tool_chain.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#64 bit tool chain for arm 
+wget https://publishing-ie-linaro-org.s3.amazonaws.com/releases/components/toolchain/binaries/7.5-2019.12/arm-linux-gnueabihf/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf.tar.xz?Signature=xhG6kpq3fINF0FRjcOKYS0UrPQ8%3D&Expires=1687544828&AWSAccessKeyId=AKIAIELXV2RYNAHFUP7A
+
+
+#once the tool chain is downloaded 
+
+
